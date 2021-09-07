e31c3343614a531b3a6a7ece9649bece890077db
DIEP NGUYEN VIET <1821050791@student.humg.edu.vn>
2021-09-07 08:34:51 +0700

    Add project files.

---
+++ ptpmqldiep/Controllers/DbcontextController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ptpmqldiep.Controllers
+{
+    public class DbcontextController : Controller
+    {
+        giaiphuongtrinh gpt = new giaiphuongtrinh();
+        // GET: Dbcontext
+        public ActionResult Index()
+        {
+            return View();
+        }
+   [HttpPost] 
+        public ActionResult Index(string soA , string soB)
+        {
+            double so1 = Convert.ToDouble(soA);
+            double so2 = Convert.ToDouble(soB);
+            double gtx = giaiphuongtrinh.Index(so1,so2);
+
+            ViewBag.ketqua = gtx;
+            return View();
+        }
+    }
+}
---
+++ ptpmqldiep/Models/giaiphuongtrinh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ptpmqldiep.Models
+{
+    public class giaiphuongtrinh
+    {
+        public Double Index(double a, double b)
+        {
+            double gtx = 0;
+            gtx = -b/a;
+            return gtx;
+        }
+    }
+}
